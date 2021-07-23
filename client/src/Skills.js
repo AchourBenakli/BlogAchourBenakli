6879e872c290022b94da6d902fa5aab6f909c21b
Achourbenakli <achourbenakli@yahoo.fr>
2021-07-23 23:56:22 +0200

    update

--- client/src/Skills.js
+++ client/src/Skills.js
@@ -17,7 +17,7 @@ export default function Skills() {
     <>
       <div className={classes.root}>
         <Typography component="h1" variant="h3" align="center" className={classes.color} gutterBottom>
-          My Skills as Circular
+          My Skills
         </Typography>
       </div>
       <Toolbar style={{ marginTop: '"0px' }} className={classes.toolbar} ></Toolbar>
